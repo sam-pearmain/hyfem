d3fe0a7e6b608cfbb2f71dfdd9ae7a0bdc166bbc
sam-pearmain <sam.pearmain@kcl.ac.uk>
2025-10-22 11:23:01 +0100

    erm

--- hyfem/core/discretisation.py
+++ hyfem/core/discretisation.py
@@ -3,22 +3,25 @@ from typing import Self, Type
 
 from hyfem.utils import *
 
-class DiscretisationScheme(Enum):
+class Discretisation(Enum):
     """The enum of all discretisation schemes"""
     ContinuousGalerkin    = auto()
     DiscontinuousGalerkin = auto()
+    PetrovGalerkin        = auto()
 
     @ClassMethod
     def from_str(cls, s: str) -> Self:
         match s.lower():
-            case "continuous galerkin" | "cg":         return cls.ContinuousGalerkin
-            case "discontinuous galerkin" | "dg":      return cls.DiscontinuousGalerkin
+            case "continuous galerkin" | "cg":    return cls.ContinuousGalerkin
+            case "discontinuous galerkin" | "dg": return cls.DiscontinuousGalerkin
+            case "petrov galerkin" | "pg":        return cls.PetrovGalerkin
             case _: raise ValueError(f"unknown discretisation scheme: {s}")
 
     def __str__(self) -> str:
         match self:
             case self.ContinuousGalerkin:    return "continous galerkin"
             case self.DiscontinuousGalerkin: return "discontinuous galerkin"
+            case self.PetrovGalerkin:        return "petrov galerkin"
 
     def __repr__(self) -> str: return str(self)
 
@@ -26,3 +29,4 @@ class DiscretisationScheme(Enum):
         match self:
             case self.ContinuousGalerkin:    return "cg"
             case self.DiscontinuousGalerkin: return "dg"
+            case self.PetrovGalerkin:        return "pg"
--- hyfem/core/eqns/eqn.py
+++ hyfem/core/eqns/eqn.py
@@ -2,43 +2,62 @@ import abc
 import ufl
 import ufl.equation
 
-from hyfem.core.discretisation import DiscretisationScheme
+from typing import Self
+
+from hyfem.core.discretisation import Discretisation
 from hyfem.core.eqns.traits import Linear, Nonlinear, Solvable
 from hyfem.utils import *
 
 
 class Equation(Solvable):
-    _discretisation_scheme: DiscretisationScheme | None
+    _discretisation_method: Discretisation | None
 
     def __init__(self) -> None:
         super().__init__()
-        self._discretisation_scheme = None
+        self._discretisation_method = None
+
+    @ClassMethod
+    def using_discretisation_method(cls, method: str) -> Self:
+        eqn = cls()
+        eqn.set_discretisation_method(method)
+        return eqn
 
     @Property
-    def discretisation_scheme(self) -> DiscretisationScheme:
-        if self._discretisation_scheme is None:
+    def discretisation_method(self) -> Discretisation:
+        if self._discretisation_method is None:
             raise RuntimeError(f"discretisation scheme not set for {type(self).__name__}")
-        return self._discretisation_scheme
+        return self._discretisation_method
+
+    @Property
+    def default_discretisation_method(self) -> Discretisation:
+        return self._default_discretisation_method_impl()
+
+    def set_discretisation_method(self, method: str) -> None:
+        self._discretisation_method = Discretisation.from_str(method)
 
     def _is_system_of_equations_impl(self) -> bool:
         return False
+    
+    @abc.abstractmethod
+    def _default_discretisation_method_impl(self) -> Discretisation:
+        ...
 
 class LinearEquation(Linear, Equation):
     @Property
     def a(self) -> ufl.Form:
-        return self._bilinear_form_impl()
+        return self._bilinear_form_dispatch()
     
     @Property
     def L(self) -> ufl.Form:
-        return self._linear_functional_impl()
+        return self._linear_functional_dispatch()
 
     @Property
     def bilinear_form(self) -> ufl.Form:
-        return self._bilinear_form_impl()
+        return self._bilinear_form_dispatch()
     
     @Property
     def linear_functional(self) -> ufl.Form:
-        return self._linear_functional_impl()
+        return self._linear_functional_dispatch()
 
     def _ufl_form_impl(self) -> ufl.Form:
         return self.a - self.L
@@ -46,29 +65,29 @@ class LinearEquation(Linear, Equation):
     def _ufl_equation_impl(self) -> ufl.equation.Equation:
         return self.a == self.L
 
-    def _bilinear_form_impl(self) -> ufl.Form:
+    def _bilinear_form_dispatch(self) -> ufl.Form:
         method = f"_{self.discretisation_scheme.abbrev()}_bilinear_form_impl"
         return getattr(self, method)()
 
-    def _linear_functional_impl(self) -> ufl.Form:
+    def _linear_functional_dispatch(self) -> ufl.Form:
         method = f"_{self.discretisation_scheme.abbrev()}_linear_functional_impl"
         return getattr(self, method)()
 
 class NonlinearEquation(Nonlinear, Equation):
     @Property
     def F(self) -> ufl.Form:
-        return self._nonlinear_form_impl()
+        return self._nonlinear_form_dispatch()
 
     @Property
     def nonlinear_form(self) -> ufl.Form:
-        return self._nonlinear_form_impl()
+        return self._nonlinear_form_dispatch()
 
     def _ufl_form_impl(self) -> ufl.Form:
-        return self._nonlinear_form_impl()
+        return self._nonlinear_form_dispatch()
     
     def _ufl_equation_impl(self) -> ufl.equation.Equation:
         return self.F == 0
 
-    def _nonlinear_form_impl(self) -> ufl.Form:
+    def _nonlinear_form_dispatch(self) -> ufl.Form:
         method = f"_{self.discretisation_scheme.abbrev()}_nonlinear_form_impl"
         return getattr(self, method)()
